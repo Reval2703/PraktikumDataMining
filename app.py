470dec60056e88585103d40523217522446859fb
Muhammad Reval Prasetya <revalprasetya136@gmail.com>
2026-06-23 17:45:12 +0700

    Fix exception handling for dataset loading

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.title("📱 Analisis Kecanduan Smartphone Menggunakan Decision Tree")
 
 df = pd.read_csv("Smartphone_Usage_And_Addiction_Analysis_7500.csv")
 st.success("Dataset berhasil dimuat")
-except Exception as e:
+
 st.error(f"Gagal membaca dataset: {e}")
 st.stop()
 
